ba95285ae62a55e6e3cf977cf5185d69d2823a2a
Babneet Singh <babneet.singh@mail.utoronto.ca>
2015-09-23 02:24:21 -0400

    Adding login and signup controllers

--- server/app.js
+++ server/app.js
@@ -4,26 +4,42 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var express_session = require('express-session');
+
+var mongoose = require('mongoose');
+var connection = mongoose.connect('mongodb://localhost/j9_irondev');
+
+require('./models/user_model.js');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var login = require('./routes/login');
+var signup = require('./routes/signup');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hjs');
+app.locals.delimiters = '<% %>';
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express_session({
+  secret: 'SECRET',
+  cookie: {maxAge: 3600*1000},
+}));
+
 app.use('/', routes);
 app.use('/users', users);
+app.use('/login', login);
+app.use('/signup', signup);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
