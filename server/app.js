692d04beed25cab939c12de7bc6b64490f04ec58
Babneet Singh <singhbabneet@gmail.com>
2015-10-03 22:01:59 -0400

    Updating CORS fix

--- server/app.js
+++ server/app.js
@@ -63,7 +63,7 @@ mongoose.set('debug', true);
 var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, Accept');
     next();
 };
 
