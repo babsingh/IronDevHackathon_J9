7247f9923b6231831fb8b09469cc7bcbc62ccd27
Babneet Singh <singhbabneet@gmail.com>
2015-10-03 21:56:52 -0400

    Updating CORS fix

--- server/app.js
+++ server/app.js
@@ -64,14 +64,7 @@ var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-
-    // intercept OPTIONS method
-    if ('OPTIONS' == req.method) {
-      res.send(200);
-    }
-    else {
-      next();
-    }
+    next();
 };
 
 // uncomment after placing your favicon in /public
